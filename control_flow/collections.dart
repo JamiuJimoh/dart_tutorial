41504a874c1b60fa33e124a5e7d3b2c7edb9345f
Jamiu Jimoh <j.jimoh@ohentpay.com>
2022-12-13 19:19:43 +0100

    collection tutorial

---
+++ control_flow/collections.dart
@@ -0,0 +1,50 @@
+void main() {
+  // [LIST]
+  // const jide = 'Jide';
+  // const jamiu = 'Jamiu';
+  // const shefiq = 'Shefiq';
+
+  final ts = ['la sdfjle', 'AL;D F', 'ALSDF '];
+  final list = <String>[
+    'Jamiu',
+    'Jimoh',
+    'Jide',
+    'Shefiq',
+    'John',
+    'aldjf',
+    ...ts
+  ];
+  const numbers = [2, 3, 45, 56, 445];
+
+  // list.add('Bosco');
+  // list.remove('Jide');
+
+  // numbers.forEach((e) {
+  //   print(e * 2);
+  // });
+
+  // for (var i = 0; i < list.length; i++) {
+  // print(list[i]);
+  // }
+
+  // for (var e in list) {
+  //   print(e);
+  // }
+
+  // [SETS]
+  // final set = <String>{'Jamiu', 'Jamiu', 'Jimoh', 'Jide', 'Shefiq', 'John'};
+
+  // [MAP]
+
+  // final map = {
+  //   'name': 'Jamiu Jimoh',
+  //   'age': 12343,
+  //   'hobby': 'football',
+  // };
+  // final m = {...map, 'name': 'sldfjsdlf'};
+  // map.forEach((key, value) {
+  //   print('key is $key, value is $value');
+  // });
+}
+  // print(list);
+// List<T>  Object
